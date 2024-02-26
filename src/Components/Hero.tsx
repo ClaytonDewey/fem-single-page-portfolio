7ae91f1df45bb04753a704453ef1f7e5cf41fb0a
Clayton Dewey <clay@dryadmedia.com>
2024-02-25 23:53:04 -0600

    style: Start hero styles

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -5,8 +5,37 @@ type HeroProps = {};
 const Hero: React.FC<HeroProps> = () => {
   return (
     <div className='hero'>
+      <img
+        src='/assets/images/pattern-circle.svg'
+        alt=''
+        className='hero__circle'
+        width='129'
+        height='129'
+      />
+      <img
+        src='/assets/images/pattern-rings.svg'
+        alt=''
+        className='hero__rings'
+        width='530'
+        height='129'
+      />
       <div className='hero__img'>
-        <img src='./assets/images/image-profile-mobile.webp' alt='Adam' />
+        <picture>
+          <source
+            media='(min-width: 62.5em)'
+            srcSet='/assets/images/image-profile-desktop.webp'
+          />
+          <source
+            media='(min-width: 37.5em)'
+            srcSet='/assets/images/image-profile-tablet.webp'
+          />
+          <img
+            src='/assets/images/image-profile-mobile.webp'
+            alt='Adam Keyes'
+            width-='174'
+            height='383'
+          />
+        </picture>
       </div>
       <div className='hero__body'>
         <h1>
