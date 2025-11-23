8152017db4038c8650c26eb0ccd00f6ee02d22c9
Clayton Dewey <clay@dryadmedia.com>
2025-11-23 09:23:13 -0600

    style: Style Hero compontent

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,6 +5,18 @@ const Hero = () => {
     <header className='hero'>
       <SocialLinks />
       <HeaderImage />
+      <div className='hero__text'>
+        <h1>
+          Nice to meet you! I&rsquo;m <span>Adam Keyes</span>.
+        </h1>
+        <p>
+          Based in the UK, I&rsquo;m a front-end developer passionate about
+          building accessible web apps that users love.
+        </p>
+        <a href='#contact' className='btn btn-primary'>
+          Contact Me
+        </a>
+      </div>
     </header>
   );
 };
